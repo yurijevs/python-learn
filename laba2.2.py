984fe5c8c7f83a816e6f94c7dadd05264d7d3e92
yurijevs <yurik.evstigneev@gmail.com>
2022-09-29 06:51:41 +0300

    Laba 2.2, laba 3

---
+++ laba2.2.py
@@ -0,0 +1,10 @@
+# Задание 1
+# Обработать строку nat таким образом, чтобы в имени интерфейса вместо FastEthernet было GigabitEthernet
+# nat = "ip nat inside source list ACL interface FastEthernet0/1 overload"
+# nat_new = nat.replace("FastEthernet", "GigabitEthernet")
+# print(nat_new)
+# Задание 2
+# Преобразовать строку mac из формата XXXX:XXXX:XXXX в формат XXXX.XXXX.XXXX
+# mac = 'AAAA:BBBB:CCCC'
+# mac_new = mac.replace(":", ".")
+# print(mac_new)
