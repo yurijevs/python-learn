c95f8ac642295e0b38cab70a0ff5a0987b9b4eb2
Yurij Evs <me@evs-yurij.ru>
2022-10-21 23:48:12 +0300

    update test_program

--- test_folder/test_program.py
+++ test_folder/test_program.py
@@ -14,17 +14,18 @@ ax.set_ylim(0, 12)
 # Since plotting a single graph
 line, = ax.plot(0, 0)
 
-def animation_function(i):
-	x.append(i * 15)
-	y.append(i)
 
-	line.set_xdata(x)
-	line.set_ydata(y)
-	return line,
+def animation_function(i):
+    x.append(i * 15)
+    y.append(i)
+
+    line.set_xdata(x)
+    line.set_ydata(y)
+    return line,
+
 
 animation = FuncAnimation(figure,
-						func = animation_function,
-						frames = np.arange(0, 10, 0.1),
-						interval = 10)
+    func=animation_function,
+    frames=np.arange(0, 10, 0.1),
+    interval=10)
 plt.show()
-
